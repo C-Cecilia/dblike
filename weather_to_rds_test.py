19bdebe271ddca96ae208c104f5fc8d3b8728767
C-Cecilia <chen.yalun@hotmail.com>
2023-03-06 15:02:56 +0000

    Create weather_to_rds_test.py

---
+++ weather_to_rds_test.py
@@ -0,0 +1,47 @@
+import requests
+import json
+import traceback
+import sqlalchemy
+from sqlalchemy import create_engine
+import traceback
+import glob
+import os
+from pprint import pprint
+import simplejson
+from IPython.display import display
+
+
+try:
+
+    respond = requests.get(
+        "https://api.openweathermap.org/data/2.5/weather?q=Dublin,ie&appid=bfc1e0afba094936525c01995c2cbbc6")
+
+    json.loads(respond.text)
+    weather_json = respond.json()
+    # time.sleep(5*60)
+    # print(weather_json)
+except:
+
+    print(traceback.format_exc())
+
+
+time = str(weather_json['dt'])
+main_weather = "\'"+weather_json['weather'][0]['main']+"\'"
+visibility = str(weather_json['visibility'])
+wind_speed = str(weather_json['wind']['speed'])
+temp = str(weather_json['main']['temp']-273.15)
+humidity = str(weather_json['main']['humidity'])
+# print(main_weather)
+
+
+URL = "dbbike.ck6m6unryigy.us-east-1.rds.amazonaws.com"
+PORT = "3306"
+DB = "dbike"
+USER = "admin"
+PASSWORD = "DBbike30830"
+
+engine = create_engine(
+    ("mysql+mysqldb://{}:{}@{}:{}/{}").format(USER, PASSWORD, URL, PORT, DB), echo=True)
+
+engine.execute("INSERT INTO weather (time, main_weather, visibility, wind_speed, temp, humidity) VALUES (" +
+               time+", "+main_weather+", "+visibility+", "+wind_speed+", "+temp+", "+humidity+");")
