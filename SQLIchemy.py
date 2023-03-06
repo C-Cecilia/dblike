e94abd267cfdbea3d056e145d21d020d57efc7b3
C-Cecilia <chen.yalun@hotmail.com>
2023-03-06 15:02:51 +0000

    Create SQLIchemy.py

---
+++ SQLIchemy.py
@@ -0,0 +1,29 @@
+import sqlalchemy as sqla
+from sqlalchemy import create_engine
+import traceback
+import glob
+import os 
+from pprint import pprint
+import simplejson as json
+import requests
+import time
+from IPython.display import display
+
+URL= "dbbike.c2pot9zcoud2.eu-west-1.rds.amazonaws.com"
+PORT="3306"
+DB="dbike"
+USER="admin"
+PASSWORD="DBbike30830" 
+
+engine = create_engine(("mysql+mysqldb://{}:{}@{}:{}/{}").format(USER,PASSWORD,URL,PORT,DB),echo=True)
+
+# sql = """ CREATE DATABASE IF NOT EXISTS dbike; """
+
+# engine.execuet(sql)
+
+res=engine.execute("Select * from station")
+print(res.fetchall())
+
+
+
+
